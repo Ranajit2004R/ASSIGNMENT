a64fff78c543388cb86e7e4d4704f77d0a795b7e
Ranajit2004R <ranajitmaity300@gmail.com>
2025-02-26 19:36:45 +0530

    aa

---
+++ Day-9/classwork/Script.js
@@ -0,0 +1,53 @@
+// let a=10;
+
+// let b=a+=20;
+// console.log(b);
+// let str="  ";
+// console.log(str.length)
+// let a="10"
+// let b="10"
+// let c= -a+-b;
+// console.log(typeof(c));
+// console.log(c);
+
+// function
+
+
+// let a=10;
+// let b=20;
+// myfunc(a,b)
+// function myfunc(a,b){
+//     console.log(a+b);
+// }
+// let a=10;
+// let b=20;
+
+// const myfunc=(w,e)=>{
+//     console.log(w+e)
+// }
+// myfunc(a,b)
+
+// hosting
+
+
+// let a=10;
+// let b=20;
+// console.log(num);
+// var num=10;
+// console.log(num);
+// console.log(myfunc);
+// myfunc(a,b)
+// function myfunc(a,b){
+//     console.log(a+b);
+// }
+
+// iife
+
+// (()=>{
+//     console.log("hello world")
+// }
+
+// )()
+
+// anonymous function
+
